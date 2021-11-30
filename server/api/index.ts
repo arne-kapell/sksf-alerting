b39ce39f6bba471d55dd9cbb022c9f2063ead228
Arne Kapell <arne.kapell@pm.me>
2021-11-30 14:59:00 +0100

    tmp logging

--- server/api/index.ts
+++ server/api/index.ts
@@ -85,7 +85,7 @@ const socketAuth = (socket: Socket, next: (err?: Error) => void) => {
 import * as dbConfig from "../db.config";
 app.use(async (req, res, next) => {
 	if (!io) {
-		console.log(dbConfig.PASSWORD.length);
+		console.log(dbConfig.PASSWORD);
 		const running = await portUsed.check(3001);
 		if (running) {
 			console.warn("Socket.io server already running, if in development mode please restart nuxt!");
