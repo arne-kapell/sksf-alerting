2e650528a8290e7b44d305aa1d35f7ff68c3e782
Akshy <akshylap@outlook.de>
2021-11-30 13:09:14 +0100

    Navbar Changes

--- components/NavBarUp.vue
+++ components/NavBarUp.vue
@@ -1,29 +1,30 @@
 <template>
   <nav>
-    <v-app-bar app color="indigo" dark>
+    <v-app-bar  color="red" dark app>
       <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title class="text-uppercase grey--text">
+      <v-toolbar-title class="text--center grey--text">
         <span>Projekt</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn color="grey">
+      <v-btn color="grey" to="/login">
         <span>Logout</span>
       </v-btn>
-      <v-btn x-medium color="warning" fab dark>
+      <v-btn color="warning" fab dark>
               <v-icon>mdi-account</v-icon>
-            </v-btn>
-    </v-app-bar>
-    <v-content>
-      <v-container class="fill-height" fluid>
-        <router-view></router-view>
-      </v-container>
-    </v-content>
+            </v-btn> 
+    </v-app-bar> 
+    
+  <!--  //<v-main>
+    //  <v-container class="fill-height" fluid>
+    //    <router-view></router-view>
+    //  </v-container>
+    //</v-main> -->
 
-    <v-navigation-drawer v-model="drawer" enable-resize-watcher temporary app class="primary ">
+    <v-navigation-drawer v-model="drawer" fixed app color="orange darker-2" dark>
       <v-list>
           <v-list-item-group>
-          <v-list-item link>
-            <v-list-item-action>
+          <v-list-item to="">
+            <v-list-item-action >
               <v-icon>mdi-home</v-icon>
             </v-list-item-action>
             <v-list-item-content>
@@ -32,15 +33,15 @@
           </v-list-item>
           <v-list-item link>
             <v-list-item-action>
-              <v-icon>mdi-home</v-icon>
+              <v-icon>mdi-view-list</v-icon>
             </v-list-item-action>
             <v-list-item-content>
               <v-list-item-title>Checkliste-Bearbeitung</v-list-item-title>
             </v-list-item-content>   
           </v-list-item>
-            <v-list-item link>
+            <v-list-item to="">
             <v-list-item-action>
-              <v-icon>mdi-home</v-icon>
+              <v-icon>mdi-wrench</v-icon>
             </v-list-item-action>
             <v-list-item-content>
               <v-list-item-title>Krisenwerkzeuge</v-list-item-title>
@@ -48,7 +49,7 @@
           </v-list-item>
             <v-list-item link>
             <v-list-item-action>
-              <v-icon>mdi-home</v-icon>
+              <v-icon>mdi-account-details</v-icon>
             </v-list-item-action>
             <v-list-item-content>
               <v-list-item-title>Account</v-list-item-title>
@@ -65,12 +66,7 @@ export default {
   data() {
     return {
       drawer: false,
-      items: [
-        {title: 'Dashboard', icon: 'mdi-view-dashboard'},
-        {title: 'Checklisten-Bearbeitung', icon: 'mdi-view-dashboard'},
-        {title: 'Krisenwerkzeuge', icon: 'mdi-help-box'},
-        {title: 'Account', icon: 'mdi-view-dashboard'}
-      ]
+
     };
   },
 };
---
+++ pages/krisenwerkzeuge.vue
@@ -0,0 +1,6 @@
+<template>
+<div class="krisenwerkzeuge">
+    Hiiiiii
+</div>
+</template>    
+
